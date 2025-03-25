188d48e28c1080817634c89e9c7520d8e3121579
Kevin Paiz Ramos <paizkevin27@gmail.com>
2025-03-25 00:01:49 -0400

    NavBar somewhat done

---
+++ my-website/src/components/navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import "../styles/navbar.css";
+import styled, { ThemeContext } from "styled-components";
+import { useContext, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">Kevin Paiz Ramos</div>
+      <ul className="navbar-links">
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/projects">Experience</Link>
+        </li>
+        <li>
+          <Link to="/contact">Projects</Link>
+        </li>
+        <li>
+          <Link to="/blog">Skills</Link>
+        </li>
+        <li>
+          <Link to="/resume">Resume</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
